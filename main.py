b6878230300722045b1d50090458e7d711bbfcde
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-03-19 21:04:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ def root():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
