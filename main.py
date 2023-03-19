7060f5de4fb6d0a5c759ca97afa196e1558f98b2
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-03-19 21:06:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ if __name__ == "__main__":
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+  
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
